d4923fc00fcf7be979a3928dc62642916fa9139f
Elmot <elmot@vaadin.com>
2018-07-20 16:27:17 +0300

    Initial commit

---
+++ src/main/java/xyz/elmot/vaadin10/qrreader/MainLayout.java
@@ -0,0 +1,53 @@
+/*
+ * Copyright 2000-2017 Vaadin Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package xyz.elmot.vaadin10.qrreader;
+
+import com.vaadin.flow.component.html.Div;
+import com.vaadin.flow.component.html.H2;
+import com.vaadin.flow.component.page.Viewport;
+import com.vaadin.flow.router.Route;
+import com.vaadin.flow.server.InitialPageSettings;
+import com.vaadin.flow.server.PageConfigurator;
+
+/**
+ * The main layout contains the header with the navigation buttons, and the
+ * child views below that.
+ */
+@SuppressWarnings("unused")
+@Viewport("width=device-width, minimum-scale=1.0, initial-scale=1.0, user-scalable=yes")
+@Route("")
+public class MainLayout extends Div
+        implements PageConfigurator {
+
+    public MainLayout() {
+        H2 title = new H2("Some demo");
+        title.addClassName("main-layout__title");
+
+        add(title);
+
+        addClassName("main-layout");
+        QRReader qrReader = new QRReader();
+
+        qrReader.setWidth("66%");
+        add(qrReader);
+    }
+
+    @Override
+    public void configurePage(InitialPageSettings settings) {
+        settings.addMetaTag("apple-mobile-web-app-capable", "yes");
+        settings.addMetaTag("apple-mobile-web-app-status-bar-style", "black");
+    }
+}
---
+++ src/main/java/xyz/elmot/vaadin10/qrreader/QRReader.java
@@ -0,0 +1,53 @@
+/*
+ * Copyright 2000-2017 Vaadin Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package xyz.elmot.vaadin10.qrreader;
+
+import com.vaadin.flow.component.AttachEvent;
+import com.vaadin.flow.component.Component;
+import com.vaadin.flow.component.Html;
+import com.vaadin.flow.component.HtmlComponent;
+import com.vaadin.flow.component.Tag;
+import com.vaadin.flow.component.Text;
+import com.vaadin.flow.component.dependency.JavaScript;
+import com.vaadin.flow.component.dependency.StyleSheet;
+import com.vaadin.flow.component.html.Div;
+import com.vaadin.flow.component.html.H2;
+import com.vaadin.flow.component.page.Viewport;
+import com.vaadin.flow.router.Route;
+import com.vaadin.flow.server.InitialPageSettings;
+import com.vaadin.flow.server.PageConfigurator;
+
+/**
+ * The main layout contains the header with the navigation buttons, and the
+ * child views below that.
+ */
+@JavaScript("/js/jsQR.js")
+@JavaScript("/js/camSupport.js")
+@StyleSheet("/css/jsQRCam.css")
+public class QRReader extends Div {
+
+    public QRReader() {
+        add(new Html("<div id='jsQRCamLoadingMessage'></div>"));
+        add(new Html("<canvas id='jsQRCamCanvas' hidden style='width:100%'></canvas>"));
+    }
+
+    @Override
+    protected void onAttach(AttachEvent attachEvent) {
+        super.onAttach(attachEvent);
+        attachEvent.getUI().getPage().executeJavaScript("jsQRCam.init()");
+    }
+
+}
